14853b7253bbb23bff01a55bdeabee9449b22fba
IoanMarghioala8 <ioan.a.marghioala@capgemini.com>
2022-05-26 12:38:19 +0300

    Upload java project with dependencies

---
+++ bakery/src/main/java/com/capgemini/bakery/taxonomy/BakeryApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini.bakery.taxonomy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BakeryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BakeryApplication.class, args);
+	}
+
+}
---
+++ bakery/src/test/java/com/capgemini/bakery/taxonomy/BakeryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgemini.bakery.taxonomy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BakeryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
