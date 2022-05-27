8ece1db0adc33d58b4baf9d1200b376b2a851ec0
IoanMarghioala8 <ioan.a.marghioala@capgemini.com>
2022-05-27 15:09:15 +0300

    Added controller

---
+++ bakery/src/main/java/com/capgemini/bakery/taxonomy/region/controller/RegionController.java
@@ -0,0 +1,20 @@
+package com.capgemini.bakery.taxonomy.region.controller;
+
+import com.capgemini.bakery.taxonomy.region.service.RegionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/bakery/v1/region")
+public class RegionController {
+
+    private final RegionService regionService;
+
+    @Autowired
+    public RegionController(RegionService regionService) {
+        this.regionService = regionService;
+    }
+
+    //TODO implement CRUD Operations
+}
