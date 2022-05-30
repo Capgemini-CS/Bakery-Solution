49d12f98f87779db890bae0c4eccaa332353f139
Boengiu <darius.a.boengiu@capgemini.com>
2022-05-30 15:02:11 +0300

    Added Area Service and Controller methods

--- src/main/java/com/capgemini/bakery/taxonomy/area/controller/AreaController.java
+++ src/main/java/com/capgemini/bakery/taxonomy/area/controller/AreaController.java
@@ -1,9 +1,14 @@
 package com.capgemini.bakery.taxonomy.area.controller;
 
+import com.capgemini.bakery.taxonomy.area.model.dto.AreaDTO;
 import com.capgemini.bakery.taxonomy.area.service.AreaService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/bakery/v1/area")
@@ -16,5 +21,28 @@ public class AreaController {
         this.areaService = areaService;
     }
 
-    //TODO implement CRUD Operations
+    @PostMapping("/")
+    public ResponseEntity<AreaDTO> postArea(@RequestBody @Valid AreaDTO areaDTO){
+        return new ResponseEntity<>(areaService.addArea(areaDTO), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<AreaDTO>> getAllAreas(){
+        return new ResponseEntity<>(areaService.getAllAreas(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AreaDTO> getAreaByID(@PathVariable Integer id){
+        return new ResponseEntity<>(areaService.getAreaByID(id),HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<AreaDTO> deleteByID(@PathVariable Integer id){
+        return new ResponseEntity<>(areaService.deleteByID(id),HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<AreaDTO> updateByID(@PathVariable Integer id, @RequestBody @Valid AreaDTO areaDTO){
+        return new ResponseEntity<>(areaService.updateAreaByID(id,areaDTO),HttpStatus.OK);
+    }
 }
--- src/main/java/com/capgemini/bakery/taxonomy/area/service/AreaService.java
+++ src/main/java/com/capgemini/bakery/taxonomy/area/service/AreaService.java
@@ -1,8 +1,14 @@
 package com.capgemini.bakery.taxonomy.area.service;
 
+import com.capgemini.bakery.taxonomy.area.model.Area;
+import com.capgemini.bakery.taxonomy.area.model.dto.AreaDTO;
+import com.capgemini.bakery.taxonomy.area.model.mapper.AreaMapper;
 import com.capgemini.bakery.taxonomy.area.repository.AreaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class AreaService {
@@ -15,4 +21,39 @@ public class AreaService {
     }
 
     //TODO implement CRUD Operations
+
+    public AreaDTO addArea(AreaDTO areaDTO){
+        Area area = areaRepository.save(AreaMapper.toArea(areaDTO));
+        return AreaMapper.toAreaDTO(area);
+    }
+
+    public List<AreaDTO> getAllAreas(){
+        return areaRepository.findAll().stream().map(AreaMapper::toAreaDTO).collect(Collectors.toList());
+    }
+
+    public AreaDTO getAreaByID(Integer areaId){
+        Area area = areaRepository.getReferenceById(areaId);
+        return AreaMapper.toAreaDTO(area);
+    }
+
+    public AreaDTO deleteByID(Integer areaId){
+        Area areaBeforeDelete = areaRepository.getReferenceById(areaId);
+        areaRepository.deleteById(areaId);
+        return AreaMapper.toAreaDTO(areaBeforeDelete);
+    }
+
+    public AreaDTO updateAreaByID(Integer areaId, AreaDTO areaDTO){
+        Area areaPreviously = new Area();
+        if(areaRepository.findById(areaId).isPresent()){
+            areaPreviously = AreaMapper.toArea(areaDTO);
+        } else {
+            System.out.println("qweqdsadczx");
+        }
+        Area areaUpdated = new Area();
+        areaUpdated.setAreaId(areaId);
+        areaUpdated.setCode(areaPreviously.getCode());
+        areaUpdated.setName(areaPreviously.getName());
+        areaRepository.save(areaUpdated);
+        return AreaMapper.toAreaDTO(areaUpdated);
+    }
 }
